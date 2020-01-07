ebbf9a36d5fa8ff8f7867174418248449c85754d
alternistom <tocy777@gmail.com>
2020-01-07 14:47:48 +0100

    Tailored to scrape NPORT-P xml files to csv

--- app/app.py
+++ app/app.py
@@ -52,40 +52,145 @@ def scrape():
 # render results to screen
 @app.route('/results')
 def results():
-    args = []
+
+
+
+    urlnostrip = request.args.get('url')
+
+    url = urlnotrip.strip()
+
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'xml')
+
+    fundName = soup.find("seriesName")
+    fundNameString = fundName.text.replace('.','').replace("/","")
+
+    totAssets = soup.find("totAssets").text
+
+    reportDate = soup.find("repPdDate")
+    reportDateString = str(reportDate.text)
+
+    regCIK = soup.find("regCik").text
+
+    onlySecs = soup.findAll("invstOrSec")
+
+    counter = 0
+
+    header = "Issue name, ISIN, Share, Value, Long-Short, Asset category, Total assets:," + str(totAssets)
+
     results = []
-    
-    for index in range(0, len(request.args.getlist('tag'))):
-        args.append({
-            'tag': request.args.getlist('tag')[index],
-            'css': request.args.getlist('css')[index],
-            'attr': request.args.getlist('attr')[index],
-        })
-    
-    response = requests.get(request.args.get('url'))
-    content = BeautifulSoup(response.text, 'lxml')
-    
-    # item to store scraped results
-    item = {}
-    
-    # loop over request arguments
-    for arg in args:
-        # store item
-        item[arg['css']] = [one.text for one in content.findAll(arg['tag'], arg['css'])]
-    
-    # loop over row indexes
-    for index in range(0, len(item[next(iter(item))])):
-        row = {}
+
+    for i in onlySecs:
+
+
+        issueNamesTextFirst = onlySecs[counter].title.text
+        issueNamesText = str(issueNamesTextFirst).replace("\n","").replace(",","")
+
+        isinTextFirst = onlySecs[counter].isin
+        isinTextStringer = str(isinTextFirst)
+        isinText = isinTextStringer.replace('<isin value="','').replace('"/>','')
+
+        if isinText == None:
+            isinText = ""
+
+        sharenumberTextFirst = onlySecs[counter].balance.text
+        shareNumberText = str(sharenumberTextFirst).replace("\n","")   
+
+        if shareNumberText == None:
+            shareNumberText = ""
+
+        valueUSDTextFirst = onlySecs[counter].valUSD.text
+        valueUSDText = str(valueUSDTextFirst).replace("\n","")
+
+        if valueUSDText == None:
+            valueUSDText = ""
+
+        payoffProfileTextFirst = onlySecs[counter].payoffProfile.text
+        payoffProfileText = str(payoffProfileTextFirst).replace("\n","")
+
+        if payoffProfileText == None:
+            payoffProfileText = ""
+
+        if onlySecs[counter].assetCat == None:
+            assetCategoryText = onlySecs[counter].assetConditional["desc"]
+        else:
+            assetCategoryTextFirst = onlySecs[counter].assetCat.text
+            assetCategoryText = str(assetCategoryTextFirst).replace("\n","")
+
+        if assetCategoryText == 'EC':
+            assetCategoryText = 'Equity-common'
+
+        if assetCategoryText == 'DBT':
+            assetCategoryText = 'Debt'
+
+        if assetCategoryText == 'STIV':
+            assetCategoryText = 'Short-term investment vehicle'
+
+        if assetCategoryText == 'ABS-MBS':
+            assetCategoryText = 'ABS-mortgage backed security'
+
+        if assetCategoryText == 'ABS-O':
+            assetCategoryText = 'ABS-other'
+
+        if assetCategoryText == 'ABS-CDBO':
+            assetCategoryText = 'ABS-collateralized bond-debt obligation'
+
+        if assetCategoryText == 'SN':
+            assetCategoryText = 'Structured note'
+
+        if assetCategoryText == 'LON':
+            assetCategoryText = 'Loan'
+
+        if assetCategoryText == 'EP':
+            assetCategoryText = 'Equity-preferred'
+
+        if assetCategoryText == 'DFE':
+            assetCategoryText = 'Derivative-foreign exchange'
+
+        if assetCategoryText == 'DIR':
+            assetCategoryText = 'Derivative-interest rate'
+
+        if assetCategoryText == 'DE':
+            assetCategoryText = 'Derivative-equity'
+
+        if assetCategoryText == None:
+            assetCategoryText = ""
+
+
+        """
+        issuerCategoryTextFirst = issuerCategory[counter]
+        issuerCategoryText = issuerCategoryTextFirst.text
+
+        if issuerCategoryText == 'CORP':
+            issuerCategoryText = 'Corporate'
+
+        if issuerCategoryText == 'MUN':
+            issuerCategoryText = 'Municipal'
+
+        if issuerCategoryText == 'RF':
+            issuerCategoryText = 'Registered fund'
+
+        if issuerCategoryText == 'USGSE':
+            issuerCategoryText = 'U.S. government sponsored entity'
+
+        if issuerCategoryText == 'USGA':
+            issuerCategoryText = 'U.S. government agency'
+
+        if issuerCategoryText == 'UST':
+            issuerCategoryText = 'U.S. Treasury'
+
+        """
+
+        args = []
         
-        # loop over stored data
-        for key, value in item.items():
-            # append value to the new row
-            row[key] = '"' + value[index] + '"'
         
-        # append new row to results list
+        row = (str(issueNamesText) + "," + isinText + "," + shareNumberText + "," + valueUSDText + "," + payoffProfileText + "," + assetCategoryText).split(",")
+
         results.append(row)
+
+        counter = counter + 1
     
-    return render_template('results.html', results=results)
+    return render_template('results.html', results=results, fundNameString=fundNameString, reportDateString=reportDateString, regCIK=regCIK, totAssets=totAssets)
 
 
 ######################################################
